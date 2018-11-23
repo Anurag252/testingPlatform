3d1e95f0a867551080feaf38a03c2dde73246cef
ADY8 <ADY8@PUN-ANURAGD02>
2018-11-23 17:18:15 +0530

    [Feature]Added timer to navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,25 +7,66 @@ import * as _ from 'lodash';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-    timeLeft : number ;
+  public  timeLeftinSeconds : number = 5 ;
+  public  timeLeftinMinutes: number = 2 ;
+  public  timeLeftinHours : number = 0 ; 
+  timer: any;
+  that : any = this;
+  
   constructor() { 
-    
+   
+    /*setInterval(() => {
+      this.timeLeftinSeconds = this.timeLeftinSeconds - 1;
+    },1000);
+
+    setInterval(() => {
+      this.timeLeftinMinutes = this.timeLeftinMinutes - 1;
+    },60000);
+
+    setInterval(() => {
+      this.timeLeftinHours = this.timeLeftinHours - 1;
+    },3600000);*/
+
+   this.timer = setInterval(this.myTimer.bind(this)  , 1000);
   }
 
     myTimer()
   {
-    console.log(this.timeLeft);
-    this.timeLeft = this.timeLeft - 1;
+   
+    
+    if(this.timeLeftinHours == 0 && this.timeLeftinMinutes == 0 && this.timeLeftinSeconds == 0)
+    {
+      clearInterval(this.timer);
+      return;
+    }
+
+
+    if(this.timeLeftinSeconds ==  0 )
+    {
+     
+      this.timeLeftinSeconds =  60;
+      if(this.timeLeftinMinutes > 0)
+      this.timeLeftinMinutes = this.timeLeftinMinutes - 1;
+    }
+   else if(this.timeLeftinMinutes == 0)
+    {
+      if(this.timeLeftinHours > 0)
+      {
+      this.timeLeftinMinutes = 59;
+      
+      this.timeLeftinHours = this.timeLeftinHours - 1;
+      }
+     
+    }
+
+    if(this.timeLeftinSeconds > 0)
+    this.timeLeftinSeconds = this.timeLeftinSeconds - 1;
   }
 
   ngOnInit() {
-   this.timeLeft = 100;
-   var timer =  setInterval(this.myTimer, 1000);
+   
   }
 
-  setInterval()
-  {
 
-  }
 
 }
