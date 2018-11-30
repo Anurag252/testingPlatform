2fae1de1fd29fc64287d6ed9303f1a6264ef4a17
Anurag <Anuragsgsits@gmail.com>
2018-12-01 01:46:19 +0530

    [FEATURE]Added quiz.component

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-quiz',
+  selector: 'quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
 
+  public jsonValue : string ='[{"option1":"Is it true1"},{"option1":"Is it true2"},{"option1":"Is it true3"}]'
+
+
+public options : any = JSON.parse(this.jsonValue);
   constructor() { }
 
   ngOnInit() {
